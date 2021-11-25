7d3092201f421ef0a60a453fdd242a86307beef5
shimonfiddler <sf817@live.mdx.ac.uk>
2021-11-25 20:23:30 +0000

    Create sentances.py

---
+++ sentances.py
@@ -0,0 +1,3 @@
+import random
+verb,noun = ['run','fart','Be','Become','Been','Being','Feel','Grow','Is','Look','Remain','Seem','Smell','Sound','Stay','Taste','Turn'],['idk','this','time','person','year','way','day','thing','man','world','life','hand','part','child','eye','woman','place','work','week','case','point','government','company','number','group','problem','fact']
+print(noun[random.randint(0,len(noun)-1)],verb[random.randint(0,len(verb)-1)])
