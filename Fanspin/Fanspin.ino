b4b7184c713a1131fc63cd4aa39d7c8a45d922b1
shimonfiddler <sf817@live.mdx.ac.uk>
2022-02-01 12:57:53 +0000

    Update Fanspin.ino

--- Fanspin/Fanspin.ino
+++ Fanspin/Fanspin.ino
@@ -1,12 +1,12 @@
 void setup() {
   // put your setup code here, to run once:
-  pinMode(13, OUTPUT);
+  pinMode(13, OUTPUT); //Pin for the fan relay
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  digitalWrite(13, HIGH);   
-  delay(3000);                      
-  digitalWrite(13, LOW);    
+  digitalWrite(13, HIGH);   //sets fan to spin
+  delay(3000);              //lets fan spin for 3 secconds        
+  digitalWrite(13, LOW);    //stops fan from spinning
   delay(5000);
 }
