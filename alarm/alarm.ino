1bc752e40cfec6bb6257a96fa2b0ec8f52bd16ac
shimonfiddler <sf817@live.mdx.ac.uk>
2022-02-01 13:00:19 +0000

    Update alarm.ino

--- alarm/alarm.ino
+++ alarm/alarm.ino
@@ -1,18 +1,18 @@
 void setup() {
   // put your setup code here, to run once:
-  pinMode(11,OUTPUT);
+  pinMode(11,OUTPUT); //output to buzzer
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
   int pwmrate;
-  pwmrate = 0;
-  while(pwmrate < 255){ 
+  pwmrate = 0; //sets the pwm to minimum
+  while(pwmrate < 255){ //loop for increasing the pwm signal gradualy
     analogWrite(11,pwmrate);
-    delay(10);
+    delay(10); //delay for each increase in pwm signal
     pwmrate = pwmrate + 1;
   }
- while(pwmrate > 0){ 
+ while(pwmrate > 0){ //loop for decreasing the pwm signal
     analogWrite(11,pwmrate);
     delay(10);
     pwmrate = pwmrate - 1;
