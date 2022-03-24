94c3e3caa0fdc56da3535db1d67f60abc972fa8e
shimonfiddler <sf817@live.mdx.ac.uk>
2022-03-24 11:07:03 +0000

    Update studentnumbsolver.py

--- studentnumbsolver.py
+++ studentnumbsolver.py
@@ -4,17 +4,19 @@ studentnumb = 'M00713267'
 Arduino = serial.Serial('/dev/ttyACM0')
 sleep(2)
 Arduino.write(studentnumb)
-print(studentnumb)
+#print(studentnumb)
 returndata = Arduino.readline()
 print(returndata)
 offset = returndata[23:25]
 offset = int(offset)
-print(offset)
+#print(offset)
+result = ''
 for i in range(8):
 	j = i+27
 	#print(returndata[j])
 	chrdec = ord(returndata[j])
 	chrdec = chrdec + offset
 	chrenc = chr(chrdec)
-	print(chrenc)
-
+	#print(chrenc)
+	result= result + chrenc
+print(result)
